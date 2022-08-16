9d30e01b6f54a3d156a0959312f125b1a4420ebe
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-08-16 23:00:26 +0100

    make enchant command support books

--- Common/src/main/java/fuzs/universalenchants/server/commands/ModEnchantCommand.java
+++ Common/src/main/java/fuzs/universalenchants/server/commands/ModEnchantCommand.java
@@ -8,6 +8,7 @@ import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import fuzs.universalenchants.UniversalEnchants;
 import fuzs.universalenchants.config.ServerConfig;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
@@ -15,14 +16,18 @@ import net.minecraft.commands.arguments.ItemEnchantmentArgument;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class ModEnchantCommand {
 	private static final DynamicCommandExceptionType ERROR_NOT_LIVING_ENTITY = new DynamicCommandExceptionType(
@@ -57,8 +62,8 @@ public class ModEnchantCommand {
 								)
 								.then(
 										// restrict this to 255, enchantment levels above 255 are not supported in vanilla and will be reset to that anyway
-										// also set min of 1, as 0 doesn't do anything
-									Commands.argument("level", IntegerArgumentType.integer(1, 255))
+										// min of 0 wouldn't do anything in vanilla, but now we use it to remove enchantments
+									Commands.argument("level", IntegerArgumentType.integer(0, 255))
 										.executes(
 											commandContext -> enchant(
 													commandContext.getSource(),
@@ -84,11 +89,35 @@ public class ModEnchantCommand {
 				ItemStack itemStack = livingEntity.getMainHandItem();
 				if (!itemStack.isEmpty()) {
 					if (UniversalEnchants.CONFIG.get(ServerConfig.class).fixEnchantCommand) {
-						// allow overriding existing enchantment level
-						if (enchantment.canEnchant(itemStack) && EnchantmentHelper.isEnchantmentCompatible(EnchantmentHelper.getEnchantments(itemStack).keySet().stream().filter(e -> e != enchantment).toList(), enchantment)) {
-							if (!replaceEnchantmentLevel(enchantment, level, itemStack)) {
-								itemStack.enchant(enchantment, level);
+						ItemStack stack = itemStack;
+						// handle books, don't forget to set the new stack to the main hand when successful
+						if (stack.getCount() == 1 && stack.is(Items.BOOK)) {
+							stack = new ItemStack(Items.ENCHANTED_BOOK);
+							CompoundTag compoundTag = itemStack.getTag();
+							if (compoundTag != null) {
+								stack.setTag(compoundTag.copy());
 							}
+						}
+						// allow overriding existing enchantment level
+						if ((stack.is(Items.ENCHANTED_BOOK) || enchantment.canEnchant(stack)) && EnchantmentHelper.isEnchantmentCompatible(EnchantmentHelper.getEnchantments(stack).keySet().stream().filter(e -> e != enchantment).toList(), enchantment)) {
+							Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(stack);
+							// when attempting to override existing enchantment level, vanilla will just add it as a duplicate
+							// this ensures the old entry is overridden instead, this method also supports removing enchantments
+							if (level > 0) {
+								enchantments.put(enchantment, level);
+							} else {
+								enchantments.remove(enchantment);
+							}
+							setEnchantments(enchantments, stack);
+							if (stack.is(Items.ENCHANTED_BOOK) && enchantments.isEmpty()) {
+								stack = new ItemStack(Items.BOOK);
+								CompoundTag compoundTag = itemStack.getTag();
+								if (compoundTag != null) {
+									stack.setTag(compoundTag.copy());
+								}
+							}
+							// don't set this to the hand before, in case enchanting isn't successful
+							livingEntity.setItemInHand(InteractionHand.MAIN_HAND, stack);
 							++successes;
 						} else if (collection.size() == 1) {
 							throw ERROR_INCOMPATIBLE.create(itemStack.getItem().getName(itemStack).getString());
@@ -113,29 +142,46 @@ public class ModEnchantCommand {
 		} else {
 			if (collection.size() == 1) {
 				commandSourceStack.sendSuccess(
-						Component.translatable("commands.enchant.success.single", enchantment.getFullname(level), collection.iterator().next().getDisplayName()), true
+						level > 0 || !UniversalEnchants.CONFIG.get(ServerConfig.class).fixEnchantCommand
+								? Component.translatable("commands.enchant.success.single", enchantment.getFullname(level), collection.iterator().next().getDisplayName())
+								: Component.translatable("commands.enchant.remove.success.single", getEnchantmentName(enchantment), collection.iterator().next().getDisplayName()), true
 				);
 			} else {
-				commandSourceStack.sendSuccess(Component.translatable("commands.enchant.success.multiple", enchantment.getFullname(level), collection.size()), true);
+				commandSourceStack.sendSuccess(
+						level > 0 || !UniversalEnchants.CONFIG.get(ServerConfig.class).fixEnchantCommand
+							? Component.translatable("commands.enchant.success.multiple", enchantment.getFullname(level), collection.size())
+							: Component.translatable("commands.enchant.remove.success.multiple", getEnchantmentName(enchantment), collection.size()), true);
 			}
 
 			return successes;
 		}
 	}
 
-	private static boolean replaceEnchantmentLevel(Enchantment enchantment, int level, ItemStack stack) {
-		// when attempting to override existing enchantment level, vanilla will just add it as a duplicate
-		// this ensures the old entry is overridden instead
-		ListTag list = stack.getEnchantmentTags();
-		ResourceLocation id = EnchantmentHelper.getEnchantmentId(enchantment);
-		for (int i = 0; i < list.size(); ++i) {
-			CompoundTag compoundtag = list.getCompound(i);
-			ResourceLocation currentId = EnchantmentHelper.getEnchantmentId(compoundtag);
-			if (currentId != null && currentId.equals(id)) {
-				list.set(i, EnchantmentHelper.storeEnchantment(id, level));
-				return true;
+	private static void setEnchantments(Map<Enchantment, Integer> map, ItemStack itemStack) {
+		// vanilla handles enchanted books much differently using EnchantedBookItem::addEnchantment
+		// we don't want that, as it doesn't allow for overriding/removing existing enchantment levels
+		ListTag list = new ListTag();
+		for (Map.Entry<Enchantment, Integer> entry : map.entrySet()) {
+			Enchantment enchantment = entry.getKey();
+			if (enchantment != null) {
+				list.add(EnchantmentHelper.storeEnchantment(EnchantmentHelper.getEnchantmentId(enchantment), entry.getValue()));
 			}
 		}
-		return false;
+		String enchantmentsKey = itemStack.is(Items.ENCHANTED_BOOK) ? EnchantedBookItem.TAG_STORED_ENCHANTMENTS : ItemStack.TAG_ENCH;
+		if (list.isEmpty()) {
+			itemStack.removeTagKey(enchantmentsKey);
+		} else {
+			itemStack.addTagElement(enchantmentsKey, list);
+		}
+	}
+
+	private static Component getEnchantmentName(Enchantment enchantment) {
+		MutableComponent mutableComponent = Component.translatable(enchantment.getDescriptionId());
+		if (enchantment.isCurse()) {
+			mutableComponent.withStyle(ChatFormatting.RED);
+		} else {
+			mutableComponent.withStyle(ChatFormatting.GRAY);
+		}
+		return mutableComponent;
 	}
 }
